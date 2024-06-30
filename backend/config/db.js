1bf480748679951099562ba703a00585d5925c11
Rahul12347 <2019rahul.maurya@ves.ac.in>
2024-06-30 17:05:49 +0530

    Fourth Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     await mongoose.connect(process.env.MONGODB_URI, {
-    dbName: 'Food-delivery'}).then(()=>console.log("DB connected"))
+    dbName: 'FoodStore'}).then(()=>console.log("DB connected"))
     .catch(error => console.log(error))
     
 }
